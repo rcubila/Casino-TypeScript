e8e61f2c6c3c593a9018531b22c590d9b581b20a
raul <raul@zipcoders-MacBook-Pro.local>
2017-12-08 12:01:13 -0500

    javaSRCfiles

---
+++ src/com/txtlearn/blackjack01/BlackPlayer.java
@@ -0,0 +1,16 @@
+package com.txtlearn.blackjack01;
+
+public class BlackPlayer {
+
+    private String name;
+    private double money;
+
+    public BlackPlayer() {
+
+    }
+
+    public BlackPlayer(String name, double money) {
+        this.name = name;
+        this.money = money;
+    }
+}
---
+++ src/com/txtlearn/blackjack01/Blackjack.java
@@ -0,0 +1,16 @@
+package com.txtlearn.blackjack01;
+
+import java.util.Scanner;
+
+public class Blackjack {
+
+	public static void main(String[] args){
+		
+		BlackJackConsole blackjack = new BlackJackConsole ();
+		blackjack.gameLogic ();
+
+
+	}
+	
+	
+}
